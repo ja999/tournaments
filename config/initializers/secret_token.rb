2b7aaeb693b11dc4869750f36b65a17b5795d5f1
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-07-04 20:52:38 +0200

    Initial commit, using Rails and mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Szkolagry::Application.config.secret_key_base = 'ed9289c35d64030eef72bd1ad583206dab62f2819e5e10a22ca5b8af1f5f275bb6a696419cf640b355f163b43d5e67c434ac95848e57d4f9d5d2dcb7568b06fc'
