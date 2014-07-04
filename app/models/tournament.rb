da70799cd2734a166cb0cadcdee37f4f915b1e89
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-07-04 23:21:58 +0200

    Add validations to tournament

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -2,8 +2,12 @@ class Tournament
   include Mongoid::Document
 
   field :name, type: String
-  field :disciplin, type: String
+  field :discipline, type: String
   field :max_participants, type: Integer
   field :time_of_event, type: DateTime
   field :deadline, type: DateTime
+
+  validates :name, :discipline, :max_participants, :time_of_event, :deadline, presence: true
+  validates :max_participants, numericality: { only_integer: true }
+  validates :name, :discipline, length: { in: 2..200 }
 end
