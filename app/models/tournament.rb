9897e0a9bf25c31237d8d6e1c75af7acff31027a
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-07-04 23:24:18 +0200

    Formatting in tournament.rb

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,11 +1,11 @@
 class Tournament
   include Mongoid::Document
 
-  field :name, type: String
-  field :discipline, type: String
+  field :name,             type: String
+  field :discipline,       type: String
   field :max_participants, type: Integer
-  field :time_of_event, type: DateTime
-  field :deadline, type: DateTime
+  field :time_of_event,    type: DateTime
+  field :deadline,         type: DateTime
 
   validates :name, :discipline, :max_participants, :time_of_event, :deadline, presence: true
   validates :max_participants, numericality: { only_integer: true }
