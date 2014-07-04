33951a6a1d3df2e332baa255f7ec9648e147f4a3
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-07-05 00:07:06 +0200

    Working new, create and params for tournaments

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,5 +1,39 @@
 class TournamentsController < ApplicationController
+  expose(:tournament, attributes: :tournament_params)
   expose :tournaments
 
   def index; end
+
+  def new; end
+
+  def create
+    tournament.creator = current_user
+    if tournament.update_attributes!(tournament_params)
+      flash[:success] = 'A tournament has been created'
+      redirect_to tournaments_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if tournament.update_attributes!(tournament_params)
+      redirect_to tournaments_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def tournament_params
+    params.require(:tournament).permit(
+      :name,
+      :discipline,
+      :max_participants,
+      :time_of_event,
+      :deadline,
+      :user_id
+    )
+  end
 end
