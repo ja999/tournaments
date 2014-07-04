366c5366aa2e1af6b27ffa3f0e7b531f94fcb4e7
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-07-04 22:35:04 +0200

    Forgot to add btsrp class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def bootstrap_class_for flash_type
+    classes = {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-info',
+      notice: 'alert-warning',
+    }
+    classes[flash_type]
+  end
 end
